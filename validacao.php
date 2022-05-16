14ff3a3bc7f759c90e50e78541c835a799da8707
Gabriel Adelungue <40636838+gabriel-adelungue@users.noreply.github.com>
2022-05-16 14:40:07 -0300

    validacoes completas

--- validacao.php
+++ validacao.php
@@ -14,7 +14,7 @@ if (empty($nome)) {
     echo "Favor preencher campo Nome";
 }  else if (is_numeric($nome)) {
     echo "Digitar somente letras";
-} else if (preg_match('/^[a-zA-Z0-9]+/', $nome) && !is_numeric($nome)) {
+} else if (preg_match('/[a-zA-Z\s]{1,}/', $nome)) {
     echo 'Nome OK!';
 } else {
     echo 'Nome tem caracteres inválidos...';
@@ -26,7 +26,7 @@ if (empty($sobreNome)) {
     echo "Favor preencher campo Sobrenome";
 } else if (is_numeric($sobreNome)) {
     echo "Digitar somente letras";
-} else if (preg_match('/^[a-zA-Z0-9]+/', $sobreNome) && !is_numeric($sobreNome)) {
+} else if (preg_match('/[a-zA-Z\s]{1,}/', $sobreNome)) {
     echo 'Sobrenome OK!';
 } else {
     echo 'Sobrenome tem caracteres inválidos...';
@@ -37,7 +37,7 @@ echo "<br>";
 if (empty($dataNascimento)) {
     echo "Favor preencher o campo Data de Nascimento";
 } else if(!preg_match('/^\d{1,2}\/\d{1,2}\/\d{4}$/', $dataNascimento)) {
-    echo "Data inválida.";
+    echo "Data inválida. Deve ser no formato XX/XX/XXXX";
 } else {
     echo "Data OK";
 }
@@ -47,8 +47,8 @@ echo "<br>";
 
 if (empty($endereco)) {
     echo "Favor digitar o Endereço";
-} else if (is_numeric($endereco)) {
-    echo "Digitar somente letras";
+} else {
+    echo "Campo endereço OK";
 }
 
 echo "<br>";
@@ -56,7 +56,7 @@ echo "<br>";
 if (empty($cep)) {
     echo "Favor preencher o campo CEP";
 } else if(!preg_match('/^[0-9]{5,5}([- ]?[0-9]{3,3})?$/', $cep)) {
-    echo "CEP inválido.";
+    echo "CEP inválido. Deve ser no formato XXXXX ou XXXXX-XXX ";
 } else {
     echo 'Cep OK';
 }
@@ -69,6 +69,10 @@ if (empty($local)) {
     echo "Favor preencher o campo Local";
 } else if (is_numeric($local)) {
     echo "Digitar somente letras";
+} else if (preg_match('/[a-zA-Z\s]{1,}/', $local)) {
+    echo "Endereço OK";
+} else {
+    echo "Endereço tem caracteres inválidos...";
 }
 
 echo "<br>";
@@ -76,7 +80,7 @@ echo "<br>";
 if (empty($telefone)) {
     echo "Favor preencher o campo Telefone";
 }  else if(!preg_match('^\(+[0-9]{2,3}\) [0-9]{5}-[0-9]{4}$^', $telefone)){
-    echo "Telefone inváildo.";
+    echo "Telefone inváildo. Deve ser no formato (XXX) XXXX-XXXX";
 } else {
     echo "Telefone OK";
 }
@@ -86,14 +90,12 @@ echo "<br>";
 
 if (empty($cidade)) {
     echo "Favor preencher o campo Cidade";
-} else if (is_numeric($cidade)) {
+} else if (is_numeric($local)) {
     echo "Digitar somente letras";
-} else if (preg_match('/^[a-zA-Z0-9]+/', $cidade) && is_numeric($cidade)) {
-    echo 'Cidade OK!';
+} else if (preg_match('/[a-zA-Z\s]{1,}/', $cidade)) {
+    echo "Cidade OK";
 } else {
-    echo 'Cidade tem caracteres inválidos...';
+    echo "Cidade tem caracteres inválidos...";
 }
-
-
 
 ?>
