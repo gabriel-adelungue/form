a4d82eb49a7d42f6f0b1d3195cc865a74de6a961
Gabriel Adelungue <40636838+gabriel-adelungue@users.noreply.github.com>
2022-05-21 13:24:03 -0300

    Update validacao.php

--- validacao.php
+++ validacao.php
@@ -1,45 +1,56 @@
 <?php
 
+include_once('./config.php');
+
 $nome = $_POST["nome"];
 $sobreNome = $_POST["sobrenome"];
 $dataNascimento = $_POST["dataNascimento"];
 $endereco = $_POST["endereco"];
 $cep = $_POST["cep"];
-$local = $_POST["local"];
+$complemento = $_POST["complemento"];
 $telefone = $_POST["telefone"];
 $cidade = $_POST["cidade"];
+$estado = $_POST["estado"];
 
 
 if (empty($nome)) {
     echo "Favor preencher campo Nome";
+    exit();
 }  else if (is_numeric($nome)) {
     echo "Digitar somente letras";
+    exit();
 } else if (preg_match('/[a-zA-Z\s]{1,}/', $nome)) {
-    echo 'Nome OK!';
+    // echo 'Nome OK!';
 } else {
     echo 'Nome tem caracteres inválidos...';
+    exit();
 }
 
 echo "<br>";
 
 if (empty($sobreNome)) {
     echo "Favor preencher campo Sobrenome";
+    exit();
 } else if (is_numeric($sobreNome)) {
     echo "Digitar somente letras";
+    exit();
 } else if (preg_match('/[a-zA-Z\s]{1,}/', $sobreNome)) {
-    echo 'Sobrenome OK!';
+    // echo 'Sobrenome OK!';
 } else {
     echo 'Sobrenome tem caracteres inválidos...';
+    exit();
 }
 
 echo "<br>";
 
 if (empty($dataNascimento)) {
     echo "Favor preencher o campo Data de Nascimento";
+    exit();
 } else if(!preg_match('/^\d{1,2}\/\d{1,2}\/\d{4}$/', $dataNascimento)) {
     echo "Data inválida. Deve ser no formato XX/XX/XXXX";
+    exit();
 } else {
-    echo "Data OK";
+    // echo "Data OK";
 }
 
 
@@ -47,42 +58,50 @@ echo "<br>";
 
 if (empty($endereco)) {
     echo "Favor digitar o Endereço";
+    exit();
 } else {
-    echo "Campo endereço OK";
+    // echo "Campo endereço OK";
 }
 
 echo "<br>";
 
 if (empty($cep)) {
     echo "Favor preencher o campo CEP";
+    exit();
 } else if(!preg_match('/^[0-9]{5,5}([- ]?[0-9]{3,3})?$/', $cep)) {
     echo "CEP inválido. Deve ser no formato XXXXX ou XXXXX-XXX ";
+    exit();
 } else {
-    echo 'Cep OK';
+    // echo 'Cep OK';
 }
 
 
 
 echo "<br>";
 
-if (empty($local)) {
-    echo "Favor preencher o campo Local";
-} else if (is_numeric($local)) {
+if (empty($complemento)) {
+    echo "Favor preencher o campo Complemento";
+    exit();
+} else if (is_numeric($complemento)) {
     echo "Digitar somente letras";
-} else if (preg_match('/[a-zA-Z\s]{1,}/', $local)) {
-    echo "Endereço OK";
+    exit();
+} else if (preg_match('/[a-zA-Z\s]{1,}/', $complemento)) {
+    // echo "Endereço OK";
 } else {
     echo "Endereço tem caracteres inválidos...";
+    exit();
 }
 
 echo "<br>";
 
 if (empty($telefone)) {
     echo "Favor preencher o campo Telefone";
+    exit();
 }  else if(!preg_match('^\(+[0-9]{2,3}\) [0-9]{5}-[0-9]{4}$^', $telefone)){
     echo "Telefone inváildo. Deve ser no formato (XXX) XXXX-XXXX";
+    exit();
 } else {
-    echo "Telefone OK";
+    // echo "Telefone OK";
 }
 
 
@@ -90,12 +109,19 @@ echo "<br>";
 
 if (empty($cidade)) {
     echo "Favor preencher o campo Cidade";
-} else if (is_numeric($local)) {
+    exit();
+} else if (is_numeric($cidade)) {
     echo "Digitar somente letras";
+    exit();
 } else if (preg_match('/[a-zA-Z\s]{1,}/', $cidade)) {
-    echo "Cidade OK";
+    // echo "Cidade OK";
 } else {
     echo "Cidade tem caracteres inválidos...";
+    exit();
 }
+
+$sql = "INSERT INTO usuarios(Nome, Sobrenome, DataNascimento, Endereco, Cep, Complemento, Telefone, Cidade, Estado) 
+VALUES ('$nome', '$sobreNome', '$dataNascimento', '$endereco', '$cep', '$complemento', '$telefone', '$cidade', '$estado')";
+$result = mysqli_query($conexao, $sql, MYSQLI_STORE_RESULT);
 
 ?>
