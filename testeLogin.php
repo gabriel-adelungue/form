375a339d50661ee6dd9a5102d956104857018b87
Gabriel Adelungue <40636838+gabriel-adelungue@users.noreply.github.com>
2022-06-30 10:55:30 -0300

    utliziando HASH como criptografia senha

--- testeLogin.php
+++ testeLogin.php
@@ -5,6 +5,8 @@
     if (isset($_POST['submit']) && !empty($_POST['nome']) && !empty($_POST['senha'])) {
         // Acessa o sistema
         include_once('config.php');
+
+        // Proteção contra SQL Injection - addslashes()
         $nome = addslashes($_POST['nome']);
         $senha = addslashes($_POST['senha']);
 
@@ -12,27 +14,38 @@
         // print_r('<br>');
         // print_r('Senha: ' . $senha);
 
-        $sql = "SELECT * FROM usuarios WHERE nome = '$nome' and senha = '$senha' ";
-
+        $sql = "SELECT * FROM usuarios WHERE nome = '$nome'";
         $result = $conexao->query($sql);
 
-        // print_r($sql);
-        // print_r($result);
-        if(mysqli_num_rows($result) < 1) {
-            // unset() — Destrói a variável especificada
-            unset($_SESSION['nome']);
-            unset($_SESSION['senha']);
-            header('Location: login.html');
-        } else {
-            // Variaveis de sessão
+        $usuario = $result->fetch_assoc();
+        if (password_verify($senha, $usuario['senha'])) {
             $_SESSION['nome'] = $nome;
             $_SESSION['senha'] = $senha;
             header('Location: sistema.php');
+        } else {
+            unset($_SESSION['nome']);
+            unset($_SESSION['senha']);
+            header('Location: login.html');
         }
-    } else {
+
+        //Código antigo sem HASH
+        // print_r($sql);
+        // print_r($result);
+        // if(mysqli_num_rows($result) < 1) {
+        //     // unset() — Destrói a variável especificada
+            // unset($_SESSION['nome']);
+            // unset($_SESSION['senha']);
+            // header('Location: login.html');
+        // } else {
+        //     // Variaveis de sessão
+            // $_SESSION['nome'] = $nome;
+            // $_SESSION['senha'] = $senha;
+            // header('Location: sistema.php');
+        // }
+    }  else {
         // Não acessa
         
         header('Location: login.html');
-
+       
     }
 ?>
